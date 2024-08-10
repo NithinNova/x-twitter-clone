02d4aff1594a215f27756878cad7e406ce7b85a9
NithinNova <nithinkumarka786@gmail.com>
2024-08-11 00:02:16 +0530

    3-Notifications query and mutation added

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,7 +5,7 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import {useMutation,useQueryClient} from "@tanstack/react-query";
+import {useMutation,useQueryClient,useQuery} from "@tanstack/react-query";
 
 const Sidebar = () => {
 	const queryClient = useQueryClient();
@@ -32,7 +32,7 @@ const Sidebar = () => {
 				},
 	})
 	
-	const {data:uthUser} = useQuery({queryKey:["authUser"]});
+	const {data:authUser} = useQuery({queryKey:["authUser"]});
 
 	return (
 		<div className='md:flex-[2_2_0] w-18 max-w-52'>
