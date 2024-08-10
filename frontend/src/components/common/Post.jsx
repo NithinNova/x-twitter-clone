e69f6d39de0c8ab8bb43eaf7ff5b23e347b953be
NithinNova <nithinkumarka786@gmail.com>
2024-08-10 12:44:33 +0530

    3-comment on post mutation updated

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,10 +8,17 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import{toast} from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const {data:authUser} = useQuery({querykey:["authUser"]});
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authuser._id);
+
+	const isMyPost = authUser._id === post.user._id;
+
+	const formattedDate = formatPostDate(post.createdAt);
 	const queryClient = useQueryClient();
 	const {mutate:deletePost, isPending:isDeleting} = useMutation({
 		mutationFn: async () => {
@@ -66,23 +73,45 @@ const Post = ({ post }) => {
 			onError: (error) => {	
 				toast.error("error liking post");
 				}
-	})
+	});
 
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authuser._id);
-
-	const isMyPost = authUser._id === post.user._id;
-
-	const formattedDate = "1h";
-
-	const isCommenting = false;
-
+	const { mutate: commentPost, isPending: isCommenting} = useMutation({
+		mutationFn: async () => {
+			try{
+				const res = await fetch(`/api/posts/comment/${post._id}/comment`,{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						},
+						body: JSON.stringify({text: comment}),
+						});
+		const data = await res.json();
+		if(res.ok){
+			throw new Error(data.error || "something went wrong");
+			}
+			return data;
+			}catch (error){
+				throw new Error(error);
+				}
+				},
+	onSuccess: (data) => {
+		toast.success("comment posted successfully");
+		setComment("");
+		queryClient.invalidateQueries({querkey:["posts"] });
+	},
+	onError: (error) => {
+		toast.error(error.message);
+	},
+	});
+	
 	const handleDeletePost = () => {
 		deletePost();
 	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if(isCommenting) return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
